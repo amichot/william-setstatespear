bde97b8fded3827b0f248a38c6d04328ca0c4a3c
Adrian <adrian_michot@yahoo.com>
2019-03-20 02:59:46 -0500

    React-Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import PoemListPage from './PoemListPage'
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import PoemListPage from "./PoemListPage";
+import PoemPage from "./PoemPage";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <nav>
-          <a href='/'>Poem List</a>
-        </nav>
-        <header>
-          <h1>William Setstatespear</h1>
-        </header>
-        <main>
-          <PoemListPage />
-        </main>
-        <footer>
-          <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
-        </footer>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<nav>
+					<Link to='/'>Poem List</Link>
+				</nav>
+				<header>
+					<h1>William Setstatespear</h1>
+				</header>
+				<main>
+					<Route exact path='/' component={PoemListPage} />
+					<Route path='/poem/:poemId' component={PoemPage} />
+				</main>
+				<footer>
+					<p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
